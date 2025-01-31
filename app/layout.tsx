0919ced74ef79eeef2bba9951275dd51db9721b5
Tarek <tarekalakmeh@hotmail.ch>
2025-01-31 15:32:29 +0000

    Initial Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>LangChain + Next.js Template</title>
+        <title>Atomic Studio</title>
         <link rel="shortcut icon" href="/images/favicon.ico" />
         <meta
           name="description"
